0b6c622d4abe63069699a14012de96fc6f82dbbb
PalabradelPadre <miguel.hernandez1hs@gmail.com>
2026-03-26 13:37:31 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,42 @@
-const CACHE = "palabra-v4";
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll([
-      "./",
-      "./index.html",
-      "./manifest.json"
-    ]))
+const CACHE_NAME = "palabra-del-padre-v5";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icono.png"
+];
+
+// INSTALACIÓN
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()) // activa inmediatamente
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+// ACTIVACIÓN
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim()) // toma control inmediato
+  );
+});
+
+// FETCH – NETWORK FIRST
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Actualiza cache con la respuesta nueva
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request)) // fallback cache
   );
 });
